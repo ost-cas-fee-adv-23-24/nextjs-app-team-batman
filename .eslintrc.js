4580e8edddd49a5e051fb22cd952d21985945cd6
Albin Hoti <onivue@gmail.com>
2023-12-19 18:51:13 +0100

    chore: update packages & add init setup

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+const config = {
+  rules: {
+    'prettier/prettier': 0,
+    'check-file/no-index': 0,
+    'newline-before-return': 0,
+    'react/forbid-component-props': ['warn', { forbid: ['style'] }],
+    'react/no-children-prop': 0,
+  },
+  // settings: {
+  //   'import/resolver': {
+  //     typescript: true,
+  //   },
+  // },
+  parser: '@typescript-eslint/parser',
+  extends: ['@smartive/eslint-config/react', 'plugin:import/typescript', 'next/core-web-vitals'],
+};
+
+module.exports = config;
