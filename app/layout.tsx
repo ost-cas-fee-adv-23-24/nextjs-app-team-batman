ff0695c7b50280d825c24ee3c21a05128ef73944
Ricardo Vögeli <ricardo.voegeli@ost.ch>
2024-01-31 14:37:37 +0100

    feat: card

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Logo } from '@ost-cas-fee-adv-23-24/design-system-component-library-team-batman';
+import { Logo, Icon } from '@ost-cas-fee-adv-23-24/design-system-component-library-team-batman';
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
@@ -17,10 +17,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={font.className}>
-        <nav className="bg-primary-600">
-          <Logo variant="white" />
+        <nav className="h-20 flex items-center justify-around bg-primary-600 md:grid md:grid-cols-12 md:content-center">
+          <div className="hidden items-center md:col-start-3 md:col-end-5 md:flex lg:col-start-4 lg:col-end-6">
+            <Logo variant="white" />
+          </div>
         </nav>
-        {children}
+        <div className="flex items-center bg-primary-600 md:hidden">
+          <Icon variant="mumble" size="l" className="m-xs fill-white" />
+        </div>
+        <main className="bg-base-100 md:grid md:grid-cols-12">
+          <div className="px-4 flex flex-col sm:mx-auto sm:w-3/4 sm:px-0 md:col-start-3 md:col-end-11 md:mx-0 md:w-auto lg:col-start-4 lg:col-end-10">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
--- demo-components/post.tsx
+++ demo-components/post.tsx
@@ -2,12 +2,12 @@ import { Post } from '@/mumble/types';
 
 export default function Post({ post }: { post: Post }) {
   return (
-    <div className="border">
+    <a className="border" href={`/post/${post.id}`}>
       {post.text && <p>{post.text}</p>}
       <div className="flex">
         <div className="mr-2">{post.likes ?? 0} likes</div>
         <div>{post.replies ?? 0} replies</div>
       </div>
-    </div>
+    </a>
   );
 }
