ecca7008325c260e5100dcc18c2fcc0c3fc02fe7
Albin Hoti <onivue@gmail.com>
2024-02-06 16:56:59 +0100

    feat: update packages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['storage.googleapis.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '**',
+      },
+    ],
   },
 };
 
