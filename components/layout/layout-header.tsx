5d8346c4bf77e38f32af5572c7f7475378a31ff2
Albin Hoti <onivue@gmail.com>
2024-02-06 16:59:40 +0100

    feat: header user link

--- components/layout/layout-header.tsx
+++ components/layout/layout-header.tsx
@@ -1,8 +1,10 @@
 import { auth } from '@/app/api/auth/[...nextauth]/auth';
 import { LayoutHeaderLoginButton, LayoutHeaderLogo, LayoutHeaderLogoutButton } from '@/components/layout';
 import { AVATAR_FALLBACK } from '@/utils/avatar-fallback';
+import { PAGE_ROUTES, RouteService } from '@/utils/route-service';
 import { Avatar } from '@ost-cas-fee-adv-23-24/design-system-component-library-team-batman';
 import NextImage from 'next/image';
+import Link from 'next/link';
 
 export const LayoutHeader = async () => {
   const session = await auth();
@@ -14,7 +16,11 @@ export const LayoutHeader = async () => {
         </div>
 
         <div className="flex items-center gap-m p-xs">
-          <Avatar size="s" image={{ alt: '', src: AVATAR_FALLBACK, as: NextImage, width: 100, height: 100 }} />
+          {session && (
+            <Link href={RouteService.page(PAGE_ROUTES.USER, { id: session?.user?.id })}>
+              <Avatar size="s" image={{ alt: '', src: AVATAR_FALLBACK, as: NextImage, width: 100, height: 100 }} />
+            </Link>
+          )}
 
           {session ? <LayoutHeaderLogoutButton /> : <LayoutHeaderLoginButton />}
         </div>
