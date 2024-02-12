2f34c109a99264eba27ef766762480c5c530170e
risuiar2023 <145476582+risuiar2023@users.noreply.github.com>
2024-02-12 16:21:46 +0100

    feat: profile (#9)
    - add styling for profile page

--- app/user/[id]/@posts/layout.tsx
+++ app/user/[id]/@posts/layout.tsx
@@ -1,23 +1,14 @@
-import { PAGE_ROUTES, RouteService } from '@/utils/route-service';
-import { Button } from '@ost-cas-fee-adv-23-24/design-system-component-library-team-batman';
-import Link from 'next/link';
 import { ReactNode, Suspense } from 'react';
+
+import MumbleUserTabGroup from '@/components/mumble/mumble-user-tabs';
 
 export default function Layout({ children, params }: { children: ReactNode; params: { id: string } }) {
   return (
-    <>
-      <nav className="grid grid-cols-2">
-        <Link href={RouteService.page(PAGE_ROUTES.USER, { id: params.id })}>
-          <Button>USER POSTS</Button>
-        </Link>
-        <Link href={RouteService.page(PAGE_ROUTES.USER_LIKED, { id: params.id })}>
-          <Button>LIKED POSTS</Button>
-        </Link>
-      </nav>
-
-      <Suspense fallback={<p>Loading ...</p>}>
-        <div className="border-2 border-r-primary-800 p-s">{children}</div>
-      </Suspense>
-    </>
+    <div className="mb-l mt-l">
+      <div className="mb-m w-[400px]">
+        <MumbleUserTabGroup id={params.id} />
+      </div>
+      <Suspense fallback={<p>Loading ...</p>}>{children}</Suspense>
+    </div>
   );
 }
--- app/user/[id]/@user/default.tsx
+++ app/user/[id]/@user/default.tsx
@@ -1,14 +1,29 @@
+import { Avatar } from '@ost-cas-fee-adv-23-24/design-system-component-library-team-batman';
+import Image from 'next/image';
+
 import { GET_USER_BY_ID } from '@/utils/api/api-actions-user';
 import { APIError } from '@/utils/api/api-service-base';
 import { notFound } from 'next/navigation';
+import { ProfileImage } from './profileImage';
+import { AVATAR_FALLBACK } from '@/utils/avatar-fallback';
 
 export default async function Page({ params }: { params: { id: string } }) {
   try {
     const user = await GET_USER_BY_ID({ id: params.id });
     return (
-      <div className="overflow-auto">
-        <h1>USER PROFILE</h1>
-        <code>{JSON.stringify(user)}</code>
+      <div className="relative">
+        <div className="mb-4 relative h-[320px] cursor-pointer overflow-hidden rounded-m bg-primary-600 object-contain">
+          <Image src={ProfileImage} alt="profile image" className="duration-200 ease-in-out hover:opacity-50" fill />
+        </div>
+        <div className="absolute bottom-[-70px] right-[30px]">
+          <Avatar
+            image={{
+              src: user.avatarUrl ? user.avatarUrl : AVATAR_FALLBACK,
+              alt: 'avatar',
+            }}
+            size="xl"
+          />
+        </div>
       </div>
     );
   } catch (error) {
