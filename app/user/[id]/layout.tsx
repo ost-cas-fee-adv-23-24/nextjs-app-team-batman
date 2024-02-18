440efae62206447eea03692a7991a2bd8da0ab26
Ricardo Vögeli <ricardo.voegeli@ost.ch>
2024-02-18 17:25:13 +0100

    feat: user-skeleton

--- app/user/[id]/layout.tsx
+++ app/user/[id]/layout.tsx
@@ -1,11 +1,12 @@
 import MumbleUserTabGroup from '@/components/mumble/mumble-user-tabs';
 import PostSkeleton from '@/components/mumble/mumble-post-skeleton';
+import UserSkeleton from '@/components/mumble/mumble-user-skeleton';
 import { ReactNode, Suspense } from 'react';
 
 export default function Layout({ user, posts, params }: { user: ReactNode; posts: ReactNode; params: { id: string } }) {
   return (
     <div className="grid w-full gap-l">
-      <Suspense fallback={<p>LOADING USER...</p>}>{user}</Suspense>
+      <Suspense fallback={<UserSkeleton />}>{user}</Suspense>
 
       <div className="mb-m w-[400px]">
         <MumbleUserTabGroup id={params.id} />
