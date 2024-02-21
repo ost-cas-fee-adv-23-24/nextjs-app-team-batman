f039917c9856d6fdd939668edc3f220854087ae4
Ricardo Vögeli <ricardo.voegeli@ost.ch>
2024-02-21 12:07:23 +0100

    fix: small improvement

--- components/mumble/mumble-user-card.tsx
+++ components/mumble/mumble-user-card.tsx
@@ -33,6 +33,7 @@ export const MumbleUserCard = ({
   const inputRef = useRef<HTMLInputElement>(null);
   const formRef = useRef<HTMLFormElement>(null);
   const router = useRouter();
+  const isOwnUser = userId === sessionUserId;
 
   const formAction = async (formData: FormData) => {
     if (sessionUserId) {
@@ -52,53 +53,55 @@ export const MumbleUserCard = ({
 
   return (
     <>
-      <form ref={formRef} action={formAction} className="grid w-full gap-s">
-        <div className="relative">
-          <div className="mb-4 relative h-[320px] cursor-pointer overflow-hidden rounded-m bg-primary-600 object-contain">
-            <Image src={ProfileImage} alt="profile image" className="duration-200 ease-in-out hover:opacity-50" fill />
-          </div>
-          <div className="absolute bottom-[-70px] right-[30px]">
-            <Avatar
-              image={{
-                src: avatarUrl ?? AVATAR_FALLBACK,
-                alt: 'avatar',
-              }}
-              size="xl"
-              onEdit={() => setModalState(!modalState)}
-              edit={userId === sessionUserId}
-            />
-          </div>
+      <div className="relative">
+        <div className="mb-4 relative h-[320px] cursor-pointer overflow-hidden rounded-m bg-primary-600 object-contain">
+          <Image src={ProfileImage} alt="profile image" className="duration-200 ease-in-out hover:opacity-50" fill />
         </div>
-        <input type="file" name="media" id="upload-media" ref={inputRef} hidden />
-        <Modal
-          isOpen={modalState}
-          onClose={() => {
-            setModalState(!modalState);
-          }}
-          onSubmit={handleClose}
-          width="m"
-          title="Bild hochladen"
-        >
-          <ImageUpload
-            id="temp-upload-media"
-            name="temp-media"
-            onChange={(event) => {
-              if (inputRef.current && event.target.files && event.target.files.length > 0) {
-                inputRef.current.files = event.target.files;
-              }
+        <div className="absolute bottom-[-70px] right-[30px]">
+          <Avatar
+            image={{
+              src: avatarUrl ?? AVATAR_FALLBACK,
+              alt: 'avatar',
             }}
+            size="xl"
+            onEdit={() => setModalState(!modalState)}
+            edit={isOwnUser}
           />
-        </Modal>
-        <div className="flex flex-col gap-xs">
-          <Label size="xl" as="h1" className="mt-m">
-            {`${firstname} ${lastname}`}
-          </Label>
-          <div className="flex flex-wrap gap-s">
-            <LinkIcon icon="profile" text={username ? username : ''} />
-            <LinkIcon icon="location" text="Switzerland" variant="secondary" />
-          </div>
         </div>
-      </form>
+      </div>
+      {isOwnUser && (
+        <form ref={formRef} action={formAction} className="grid w-full gap-s">
+          <input type="file" name="media" id="upload-media" ref={inputRef} hidden />
+          <Modal
+            isOpen={modalState}
+            onClose={() => {
+              setModalState(!modalState);
+            }}
+            onSubmit={handleClose}
+            width="m"
+            title="Bild hochladen"
+          >
+            <ImageUpload
+              id="temp-upload-media"
+              name="temp-media"
+              onChange={(event) => {
+                if (inputRef.current && event.target.files && event.target.files.length > 0) {
+                  inputRef.current.files = event.target.files;
+                }
+              }}
+            />
+          </Modal>
+          <div className="flex flex-col gap-xs">
+            <Label size="xl" as="h1" className="mt-m">
+              {`${firstname} ${lastname}`}
+            </Label>
+            <div className="flex flex-wrap gap-s">
+              <LinkIcon icon="profile" text={username ? username : ''} />
+              <LinkIcon icon="location" text="Switzerland" variant="secondary" />
+            </div>
+          </div>
+        </form>
+      )}
     </>
   );
 };
