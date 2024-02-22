0402db63a56e0c8448988ab6c77faba56a084772
Ricardo Vögeli <ricardo.voegeli@ost.ch>
2024-02-22 14:56:48 +0100

    fix: code-review-fixes

--- components/layout/layout-header.tsx
+++ components/layout/layout-header.tsx
@@ -4,11 +4,11 @@ import { PAGE_ROUTES, RouteService } from '@/utils/route-service';
 import { Avatar } from '@ost-cas-fee-adv-23-24/design-system-component-library-team-batman';
 import NextImage from 'next/image';
 import Link from 'next/link';
-import { useGetUserAvatar } from '@/app/user/hooks/useGetUserAvatar';
+import { userAvatar } from '@/utils/user-avatar';
 
 export const LayoutHeader = async () => {
   const session = await auth();
-  const userAvatar = await useGetUserAvatar(session?.user?.id);
+  const avatar = await userAvatar(session?.user?.id);
 
   return (
     <header className="flex w-full place-content-center bg-primary-700">
@@ -20,7 +20,7 @@ export const LayoutHeader = async () => {
         <div className="flex items-center gap-m p-xs">
           {session && (
             <Link href={RouteService.page(PAGE_ROUTES.USER, { id: session?.user?.id })}>
-              <Avatar size="s" image={{ alt: '', src: userAvatar, as: NextImage, width: 100, height: 100 }} />
+              <Avatar size="s" image={{ alt: '', src: avatar, as: NextImage, width: 100, height: 100 }} />
             </Link>
           )}
 
--- utils/user-avatar.ts
+++ utils/user-avatar.ts
@@ -1,7 +1,7 @@
 import { GET_USER_BY_ID } from '@/utils/api/api-actions-user';
 import { AVATAR_FALLBACK } from '@/utils/avatar-fallback';
 
-export const useGetUserAvatar = async (id: string | undefined) => {
+export const userAvatar = async (id: string | undefined) => {
   if (id === undefined) {
     return AVATAR_FALLBACK;
   }
