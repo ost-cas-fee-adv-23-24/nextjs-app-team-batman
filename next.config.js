303e63cc04b3e1d878ce36b4134fbf0973988757
Albin Hoti <onivue@gmail.com>
2024-02-22 09:45:44 +0100

    feat: pipelines
    - add github action pipelines (lint, typecheck, build & e2e)
    - initialize playwright for e2e testing

--- next.config.js
+++ next.config.js
@@ -14,9 +14,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    NEXTAUTH_URL: process.env.DEPLOY_PRIME_URL || process.env.URL || 'http://localhost:3000',
-  },
 };
 
 module.exports = nextConfig;
