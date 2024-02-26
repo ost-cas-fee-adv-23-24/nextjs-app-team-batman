e11a105ed04711f4b3143dacb1f4832e12aeb856
Ricardo Vögeli <ricardo.voegeli@ost.ch>
2024-02-26 07:33:49 +0100

    feat: first and lastname from users added in posts

--- components/mumble/mumble-post.tsx
+++ components/mumble/mumble-post.tsx
@@ -1,7 +1,7 @@
 import { auth } from '@/app/api/auth/[...nextauth]/auth';
 import { MUMBLE_USER_INFO_VARIANT, MumbleUserInfo } from '@/components/mumble';
 import { decodeULIDTimestamp } from '@/utils/api/api-helpers';
-import { TAPIPost } from '@/utils/api/api-types';
+import { TAPIPost, TAPIUser } from '@/utils/api/api-types';
 import { PAGE_ROUTES, RouteService } from '@/utils/route-service';
 import { CommentButton, Image } from '@ost-cas-fee-adv-23-24/design-system-component-library-team-batman';
 import NextImage from 'next/image';
@@ -9,6 +9,11 @@ import Link from 'next/link';
 import { MumbleCopy } from './mumble-copy';
 import { MumbleDelete } from './mumble-delete';
 import { MumbleLike } from './mumble-like';
+import { getUser } from '@/utils/user';
+
+const parseUserFirstLastName = (user: Pick<TAPIUser, 'firstname' | 'lastname'>) => {
+  return user?.firstname && user?.lastname ? `${user?.firstname} ${user?.lastname}` : 'First Lastname';
+};
 
 export const MumblePost = async ({
   post,
@@ -18,12 +23,18 @@ export const MumblePost = async ({
   variant: MUMBLE_USER_INFO_VARIANT.DETAILVIEW | MUMBLE_USER_INFO_VARIANT.TIMELINE;
 }) => {
   const session = await auth();
+  const user = await getUser(post.creator.id ? post.creator.id : undefined);
+
+  let displayname = 'First Lastname';
+  if (user) {
+    displayname = parseUserFirstLastName(user);
+  }
 
   return (
     <div className="grid gap-m">
       <MumbleUserInfo
         variant={variant}
-        displayname="First Name"
+        displayname={displayname}
         userId={post.creator.id!}
         username={post.creator.username!}
         date={decodeULIDTimestamp(post.id)}
