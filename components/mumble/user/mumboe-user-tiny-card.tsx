5a16719955fdbe0cd69a0c98de66679b8c950d8f
Ricardo Vögeli <ricardo.voegeli@ost.ch>
2024-03-08 17:12:11 +0100

    feat: link-to-user-profile-added

--- components/mumble/user/mumboe-user-tiny-card.tsx
+++ components/mumble/user/mumboe-user-tiny-card.tsx
@@ -4,6 +4,8 @@ import { Avatar, Label, Button } from '@ost-cas-fee-adv-23-24/design-system-comp
 import { AVATAR_FALLBACK } from '@/app/app-config';
 import { UPDATE_USERS_UNFOLLOW } from '@/utils/api/api-actions-user';
 import { useRouter } from 'next/navigation';
+import { PAGE_ROUTES, RouteService } from '@/utils/route-service';
+import Link from 'next/link';
 
 interface IMumbleUserTinyCard {
   id: string;
@@ -22,20 +24,22 @@ export const MumbleUserTinyCard = ({ avatarUrl, id, firstname, lastname, buttonT
   };
   return (
     <div className={cn('relative h-[242px] w-full items-center rounded-m bg-white p-s sm:w-[216px]')}>
-      <div className="flex items-center justify-center">
-        <Avatar
-          image={{
-            src: avatarUrl ?? (AVATAR_FALLBACK as string),
-            alt: 'avatar',
-          }}
-          size="l"
-        />
-      </div>
-      <div className="items-center justify-center truncate pt-s text-center">
-        <Label size="xl">
-          {firstname} {lastname}
-        </Label>
-      </div>
+      <Link href={RouteService.page(PAGE_ROUTES.USER, { id })}>
+        <div className="flex items-center justify-center">
+          <Avatar
+            image={{
+              src: avatarUrl ?? (AVATAR_FALLBACK as string),
+              alt: 'avatar',
+            }}
+            size="l"
+          />
+        </div>
+        <div className="items-center justify-center truncate pt-s text-center">
+          <Label size="xl">
+            {firstname} {lastname}
+          </Label>
+        </div>
+      </Link>
       <div className="flex justify-center pt-s">
         <Button size="m" variant="primary" onClick={handleUnfollow} fullWidth>
           {buttonText}
